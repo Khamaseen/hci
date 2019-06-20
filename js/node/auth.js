b56876529a513ff8b183569a7c045bed1cff99cd
Khamaseen <dennisgijzen@gmail.com>
2019-06-20 21:56:12 +0200

    more info on logging in/loggin out

--- js/node/auth.js
+++ js/node/auth.js
@@ -1,3 +1,6 @@
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const _Lodash = require('lodash');
 const bcrypt = require('bcrypt');
 const {User} = require('../node/user');
@@ -9,6 +12,16 @@ const bcrypt = require('bcrypt');
 
 run();
 
+// a webtoken is send inside the header.
+// a webtoken is created with a private key in combination with some user info
+// since the header should be secure in https it is hard to get the webtoken for other users
+// logging out a user is done on the client side.. this leaves the jwt still valid
+// a valid jwt can be used by anybody who has their hands on it
+// experiation times for a jwt are reasonable.. blacklisting a jwt when they are logged out too.
+// still basically jwt means: "one does not simply log out with jwt"
+
+
+// validation of user with encrypted password
 router.post('/', async (req, res) => {
     const { error } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -19,10 +32,11 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('invalid email or password');
 
-    res.send(true);
+    const token = user.generateAuthToken();
+    res.send(token);
 });
 
-
+// authentication of a user
 function validateUser(req) {
     const schema = {
         email: Joi.string().min(5).max(255).required().email(),
