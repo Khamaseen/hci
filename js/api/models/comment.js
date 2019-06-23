edd976aa730327b3d8f44df6237364624401479f
Detjon Shahu <sh.detion@gmail.com>
2019-06-23 19:07:08 +0200

    +comment, linked to post

---
+++ js/api/models/comment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    username: {
+        type: String, 
+        required: true
+    },
+    paragraph: {
+        type: String, 
+        required: true
+    },
+    post:{
+        _id: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
